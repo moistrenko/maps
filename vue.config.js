1f2123daa4a63f7cf9e018bb170378767b6d5e10
Anatoly Moistrenko <moistrenko@mail.ru>
2023-04-07 18:42:57 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
-  transpileDependencies: true,
   assetsDir: "assets",
+  transpileDependencies: true,
   productionSourceMap: false,
 });
